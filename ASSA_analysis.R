d653410da8425bf48f68ee5c99551addb551672f
Bulat Murtazin <55959925+argentum049@users.noreply.github.com>
2021-06-29 09:52:01 +0300

    Add files via upload
    Новые строки в файлах assa_analysis.* + профили сайтов связывания

--- ASSA_analysis.R
+++ ASSA_analysis.R
@@ -1,51 +1,107 @@
-source("lib2.R")
-library(seqinr)
-
-
+source("lib.R")
 
 assa_07 <- read.csv("output_07.txt", row.names = 1, sep = "\t")
-assa_pvalue_07 <- assa_07 %>%
+assa_07 <- assa_07 %>%
   filter(Pvalue<0.01)
-
-assa_10 <- read.csv("output_10.txt", row.names = 1, sep = "\t")
-assa_pvalue_10 <- assa_10 %>%
-  filter(Pvalue<0.01)
-
 
 rnaup_07 <- read.table("rnaup_sites_07.txt",row.names = 1)
 colnames(rnaup_07) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
 rnaup_07 <- rnaup_07 %>%
-  filter(name2 %in% assa_pvalue_07$name2)
+  filter(name2 %in% assa_07$name2) %>%
+  filter(deltaG<0)
+rnaup_07$Pvalue <- assa_07$Pvalue[match(rnaup_07$name2,assa_07$name2)]
+rnaup_07$Padj <- assa_07$Padj[match(rnaup_07$name2,assa_07$name2)]
+write.table(rnaup_07,"rnaup_sites_07_only_values.txt",quote=F,sep = "\t")
+
+
+assa_10 <- read.csv("output_10.txt", row.names = 1, sep = "\t")
+assa_10 <- assa_10 %>%
+  filter(Pvalue<0.01)
 
 rnaup_10 <- read.table("rnaup_sites_10.txt",row.names = 1)
 colnames(rnaup_10) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
 rnaup_10 <- rnaup_10 %>%
-  filter(name2 %in% assa_pvalue_10$name2)
+  filter(name2 %in% assa_10$name2) %>%
+  filter(deltaG<0)
+rnaup_10$Pvalue <- assa_10$Pvalue[match(rnaup_10$name2,assa_10$name2)]
+rnaup_10$Padj <- assa_10$Padj[match(rnaup_10$name2,assa_10$name2)]
+write.table(rnaup_10,"rnaup_sites_10_only_values.txt",quote=F,sep = "\t")
 
-assa_07_wo_first <- read.csv("output_07_wo_first.txt", row.names = 1, sep = "\t")
-assa_pvalue_07_wo_first <- assa_07_wo_first %>%
+rnaup_total_mouse <- rbind(rnaup_07,rnaup_10)
+rnaup_total_mouse <- rnaup_total_mouse %>%
+  mutate(
+    key=paste(name2, siteStart1)
+  )
+rnaup_total_mouse <- rnaup_total_mouse %>%
+  distinct(key, .keep_all = TRUE)
+write.table(rnaup_total_mouse,"rnaup_total_mouse.txt",quote=F,sep = "\t")
+
+
+
+
+
+
+human_assa_07 <- read.csv("output_hg_07.txt",row.names = 1, sep = "\t")
+human_assa_07 <- human_assa_07 %>%
   filter(Pvalue<0.01)
 
-assa_10_wo_first <- read.csv("output_10_wo_first.txt", row.names = 1, sep = "\t")
-assa_pvalue_10_wo_first <- assa_10_wo_first %>%
+rnaup_hg_07 <- read.table("hg_sites_07.txt", row.names = 1)
+colnames(rnaup_hg_07) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
+rnaup_hg_07 <- rnaup_hg_07 %>%
+  filter(name2 %in% human_assa_07$name2) %>%
+  filter(deltaG<0)
+rnaup_hg_07$Pvalue <- human_assa_07$Pvalue[match(rnaup_hg_07$name2,human_assa_07$name2)]
+rnaup_hg_07$Padj <- human_assa_07$Padj[match(rnaup_hg_07$name2,human_assa_07$name2)]
+write.table(rnaup_hg_07,"hg_sites_07_only_values.txt",quote=F,sep = "\t")
+
+
+human_assa_10 <- read.csv("output_hg_10.txt",row.names = 1, sep = "\t")
+human_assa_10 <- human_assa_10 %>%
   filter(Pvalue<0.01)
 
+rnaup_hg_10 <- read.table("hg_sites_10.txt", row.names = 1)
+colnames(rnaup_hg_10) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
+rnaup_hg_10 <- rnaup_hg_10 %>%
+  filter(name2 %in% human_assa_10$name2) %>%
+  filter(deltaG<0)
+rnaup_hg_10$Pvalue <- human_assa_10$Pvalue[match(rnaup_hg_10$name2,human_assa_10$name2)]
+rnaup_hg_10$Padj <- human_assa_10$Padj[match(rnaup_hg_10$name2,human_assa_10$name2)]
+write.table(rnaup_hg_10,"hg_sites_10_only_values.txt",quote=F,sep = "\t")
 
-rnaup_07_wo_first <- read.table("rnaup_sites_07_wo_first.txt",row.names = 1)
-colnames(rnaup_07_wo_first) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
-rnaup_07_wo_first <- rnaup_07_wo_first %>%
-  filter(name2 %in% assa_pvalue_07_wo_first$name2)
-
-rnaup_10_wo_first <- read.table("rnaup_sites_10_wo_first.txt",row.names = 1)
-colnames(rnaup_10_wo_first) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
-rnaup_10_wo_first <- rnaup_10_wo_first %>%
-  filter(name2 %in% assa_pvalue_10_wo_first$name2)
-
+rnaup_total_human <- rbind(rnaup_hg_07,rnaup_hg_10)
+rnaup_total_human <- rnaup_total_human %>%
+  mutate(
+    key=paste(name2, siteStart1)
+  )
+rnaup_total_human <- rnaup_total_human %>%
+  distinct(key, .keep_all = TRUE)
+write.table(rnaup_total_human,"rnaup_total_human.txt",quote=F,sep = "\t")
 
 
 
 
 
+
+
+
+#assa_07_wo_first <- read.csv("output_07_wo_first.txt", row.names = 1, sep = "\t")
+#assa_pvalue_07_wo_first <- assa_07_wo_first %>%
+#  filter(Pvalue<0.01)
+#
+#assa_10_wo_first <- read.csv("output_10_wo_first.txt", row.names = 1, sep = "\t")
+#assa_pvalue_10_wo_first <- assa_10_wo_first %>%
+#  filter(Pvalue<0.01)
+#
+#
+#rnaup_07_wo_first <- read.table("rnaup_sites_07_wo_first.txt",row.names = 1)
+#colnames(rnaup_07_wo_first) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
+#rnaup_07_wo_first <- rnaup_07_wo_first %>%
+#  filter(name2 %in% assa_pvalue_07_wo_first$name2)
+#
+#rnaup_10_wo_first <- read.table("rnaup_sites_10_wo_first.txt",row.names = 1)
+#colnames(rnaup_10_wo_first) <- c("name1",	"name2",	"siteStart1",	"siteEnd1",	"siteStart2",	"siteEnd2",	"siteLen",	"siteCmpl",	"siteScore",	"regStart1",	"regLen1",	"regStart2",	"regLen2",	"regGC",	"deltaG")
+#rnaup_10_wo_first <- rnaup_10_wo_first %>%
+#  filter(name2 %in% assa_pvalue_10_wo_first$name2)
 
 
 
